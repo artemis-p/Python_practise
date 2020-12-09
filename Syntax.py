d883f0a70636b79e230adf9fbceba7d34a72a247
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-12-09 14:19:26 +0000

    Syntax

---
+++ Syntax.py
@@ -0,0 +1,52 @@
+my_name = "artemis"
+print("hello " + my_name)
+
+# this is a comment
+
+print("Hello world!")
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "sausages"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "salad"
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+release_year = 1998
+runtime = 2
+rating_out_of_10 = 8.5
+
+print(25 * 68 + 13 / 28)
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2)
+# 7x7 quilt
+print(7 ** 2)
+# 8x8 quilt
+print(8 ** 2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print(6 ** 4)
+
+birthday_string = "I am "
+age = 10
+birthday_string_2 = " years old today!"
+full_birthday_string = birthday_string + str(age) + birthday_string_2
+# Concatenating an integer with strings is possible if we turn the integer into a string first
+
+# By using three quote-marks (""" or ''') instead of one, we tell the program that the string doesn’t end until the next triple-quote. 
+
