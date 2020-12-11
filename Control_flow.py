8fce4a907a1d4e03f383aa3fcd5582beaf3c2842
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-12-11 10:42:52 +0000

    Control flow practise

--- Control_flow.py
+++ Control_flow.py
@@ -34,3 +34,11 @@ try:
   raises_value_error()
 except:
   print("You raised a ValueError!")
+
+def applicant_selector(gpa, ps_score, ec_count):
+    if (gpa >= 3.0) and (ps_score >= 90) and (ec_count >=3):
+        return "This applicant should be accepted."
+    elif (gpa >= 3.0) and (ps_score >= 90):
+        return "This applicant should be given an in-person interview."
+    else:
+        return "This applicant should be rejected."
