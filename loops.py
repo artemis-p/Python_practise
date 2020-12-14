0ed2f5a505eece35cbd249a785e596a454373b8c
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-12-14 17:07:04 +0000

    While loops and nested loops

--- loops.py
+++ loops.py
@@ -12,11 +12,38 @@ for student in students_period_A:
     print(student)
 
 
-dog_breeds_available_for_adoption = ['french_bulldog', 'dalmatian', 'shihtzu', '    poodle', 'collie']
+dog_breeds_for_adoption = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
 dog_breed_I_want = 'dalmatian'
 
-for dog in dog_breeds_available_for_adoption:
+for dog in dog_breeds_for_adoption:
     print(dog)
     if dog == dog_breed_I_want:
         print("They have the dog I want!")
         break
+
+
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+
+for age in ages:
+    if age < 21:
+        continue
+    else:
+        print(age)
+
+
+all_students = ["Alex", "Briana", "Cheri", "Daniele", "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
+students_in_poetry = []
+
+while len(students_in_poetry) < 6:
+    student = all_students.pop()
+    students_in_poetry.append(student)
+  
+print(students_in_poetry)
+
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+scoops_sold = 0
+
+for location in sales_data:
+    for sales in location:
+        scoops_sold += sales
+        print(scoops_sold)
