187754c2c0143d3694f1c9e4627b1d16c79252cc
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-12-15 10:50:44 +0000

    Sum squares kata with loops

--- loops.py
+++ loops.py
@@ -47,3 +47,20 @@ for location in sales_data:
     for sales in location:
         scoops_sold += sales
         print(scoops_sold)
+
+
+# it returns the sum of all the squares of numbers between 0 and x (not
+# included). Remember that you can use the range(x) function to generate a
+# sequence of numbers from 0 to x (not included).
+
+
+def square(n):
+    return n*n
+
+
+def sum_squares(x):
+    sum = 0
+    for n in range(x):
+        sum += square(n)
+    return sum
+    
