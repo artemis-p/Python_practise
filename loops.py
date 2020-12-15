2611cb273cd0839ec281c8585771f8a1f3ec055a
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-12-15 11:51:00 +0000

    Calling a function with a list and not a string - kata

--- loops.py
+++ loops.py
@@ -82,3 +82,21 @@ print(factorial(4))
 # should return 24
 print(factorial(5))
 # should return 120
+
+
+# The validate_users function is used by the system to check if a list of
+# users is valid or invalid. A valid user is one that is at least 3 characters
+# long. For example, ['taylor', 'luisa', 'jamaal'] are all valid users. When
+# calling it like in this example, something is not right. Can you figure out
+# what to fix?
+
+
+def validate_users(users):
+    for user in users:
+        if is_valid(user):
+            print(user + " is valid")
+        else:
+            print(user + " is invalid")
+
+
+validate_users(["purplecat"])
