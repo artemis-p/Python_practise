333042709c519e3d4933761177c805c99351ed35
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-12-16 15:37:55 +0000

    Larger sum

--- Challenges_loops.py
+++ Challenges_loops.py
@@ -84,4 +84,24 @@ def exponents(bases, powers):
         for power in powers:
             new_bases.append(base ** power)
     return new_bases
-    
+
+
+# Create a function named larger_sum() that takes two lists of numbers as
+# parameters named lst1 and lst2.
+
+# The function should return the list whose elements sum to the greater number.
+# If the sum of the elements of each list are equal, return lst1.
+
+def larger_sum(lst1, lst2):
+    sum_lst1 = sum(lst1)
+    sum_lst2 = sum(lst2)
+
+    if sum_lst1 > sum_lst2:
+        return lst1
+    elif sum_lst1 == sum_lst2:
+        return lst1
+    else:
+        return lst2
+
+
+print(larger_sum([1, 9, 5], [2, 3, 7]))
