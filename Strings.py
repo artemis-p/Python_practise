6e8e23ae81e9b155fbc47b5e83e58213269013f0
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-12-17 11:48:40 +0000

    Indices, string methods and formatting a string

--- Strings.py
+++ Strings.py
@@ -16,3 +16,27 @@ def first_and_last(message):
 print(first_and_last("else"))
 print(first_and_last("tree"))
 print(first_and_last(""))
+
+word = "supercalifragilisticexpialidocious"
+letter_x = word.index("x")
+print(letter_x)
+
+
+def initials(phrase):
+    words = phrase.split()
+    result = ""
+    for word in words:
+        result += word[0]
+    return result.upper()
+
+print(initials("Universal Serial Bus")) # Should be: USB
+print(initials("local area network")) # Should be: LAN
+print(initials("Operating system")) # Should be: OS
+
+def student_grade(name, grade):
+	message = "{} received {}% on the exam".format(name, grade)
+	return message
+
+print(student_grade("Reed", 80))
+print(student_grade("Paige", 92))
+print(student_grade("Jesse", 85))
