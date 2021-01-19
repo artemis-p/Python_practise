dc1668f728b180f4c7dce891c32aeab7eec8cf32
Artemis Papanikolaou <artemispapanik@gmail.com>
2021-01-19 14:15:52 +0000

    Return the index of equal items

--- Challenges_loops.py
+++ Challenges_loops.py
@@ -148,3 +148,19 @@ def max_num(nums):
     return max(nums)
 
 print(max_num([50, -10, 0, 75, 20]))
+
+
+# Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+
+# The function should return a list of the indices where the values were equal in lst1 and lst2.
+
+# For example, the following code should return [0, 2, 3]
+
+def same_values(lst1, lst2):
+  equal_items = []
+  for index in range(len(lst1)):
+    if lst1[index] == lst2[index]:
+      equal_items.append(index)
+  return equal_items
+
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
