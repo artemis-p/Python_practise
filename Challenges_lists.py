0c0e0dad26f78707c1aa73db4a67f78c59b89294
Artemis Papanikolaou <artemispapanik@gmail.com>
2021-01-21 13:40:32 +0000

    Append sum

--- Challenges_lists.py
+++ Challenges_lists.py
@@ -13,3 +13,24 @@ def append_size(lst):
 
 
 print(append_size([23, 42, 108]))
+
+
+
+# Write a function named append_sum that has one parameter — a list named
+# named lst.
+
+# The function should add the last two elements of lst together and append the
+# result to lst. It should do this process three times and then return lst.
+
+# For example, if lst started as [1, 1, 2], the final result should be
+# [1, 1, 2, 3, 5, 8].
+
+
+def append_sum(lst):
+  lst.append(lst[-2] + lst[-1])
+  lst.append(lst[-2] + lst[-1])
+  lst.append(lst[-2] + lst[-1])
+  return lst
+
+
+print(append_sum([1, 1, 2]))
