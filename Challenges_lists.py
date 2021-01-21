cb061de62d223afcf8a015ea280e3b7fc523d2d1
Artemis Papanikolaou <artemispapanik@gmail.com>
2021-01-21 14:16:11 +0000

    Every three numbers

--- Challenges_lists.py
+++ Challenges_lists.py
@@ -81,3 +81,19 @@ def combine_sort(lst1, lst2):
 
 
 print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+# Create a function called every_three_nums that has one parameter named start.
+
+# The function should return a list of every third number between start and 100
+# (inclusive). For example, every_three_nums(91) should return the list
+# [91, 94, 97, 100]. If start is greater than 100, the function should return
+# an empty list.
+
+def every_three_nums(start):
+  if (start > 100):
+    return []
+  else:
+    return list(range(start, 101, 3))
+
+
+print(every_three_nums(101))
