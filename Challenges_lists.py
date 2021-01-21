58cea3fb471583b15d806388ba535676c9c8f35e
Artemis Papanikolaou <artemispapanik@gmail.com>
2021-01-21 13:59:46 +0000

    Combine sort

--- Challenges_lists.py
+++ Challenges_lists.py
@@ -66,3 +66,18 @@ def more_than_n(lst, item, n):
 
 
 print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+
+
+# Write a function named combine_sort that has two parameters named lst1 and
+# lst2.
+
+# The function should combine these two lists into one new list and sort the
+# result. Return the new sorted list.
+
+def combine_sort(lst1, lst2):
+  combined_list = lst1 + lst2
+  combined_list.sort()
+  return combined_list
+
+
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
